c70d81e6a28f591352892c2add37dbbb4398255c
Bruno Munoz <bruno.munoz@orange.com>
2021-09-29 15:32:55 +0200

    add krew plugin and ctx and ns

---
+++ kubectx/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"${OS}_${ARCH}" &&
+  "$KREW" install krew
+)
+
+# install kubectx
+kubectl krew install ctx
+# install kubens
+kubectl krew install ns
