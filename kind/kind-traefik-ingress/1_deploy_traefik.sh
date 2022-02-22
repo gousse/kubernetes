49f005a89ef7dc550bd7ed465b79b92585eb1600
Bruno Munoz <bruno.munoz@orange.com>
2022-02-22 17:17:48 +0100

    update values

--- kind/kind-traefik-ingress/1_deploy_traefik.sh
+++ kind/kind-traefik-ingress/1_deploy_traefik.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-
 # create namespace
 kubectl create namespace traefik --dry-run=client -o yaml | kubectl apply -f -
+
+# generate certificates
+bash ./certs.sh
 
 # helm traefik
 helm upgrade --install -n traefik traefik traefik/traefik -f traefik-values.yaml
 # add a traefic monitoring service
 kubectl apply -n traefik -f monitoring-traefik.yaml
 
+# add default cert store
+kubectl apply -n traefik -f default-cert.yaml
+
 echo "Waiting for pods to be ready"
 kubectl wait --namespace traefik deploy/traefik --for condition=available
+
+pod=$(kubectl get pods -n traefik -l app.kubernetes.io/instance=traefik -o jsonpath='{.items[0].metadata.name}')
+kubectl -n traefik wait pod/${pod} --for condition=ready --timeout 120s
