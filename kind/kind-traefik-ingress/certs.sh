39d71ca5f6961d85b2b3a0dcd78581705a89458a
Bruno Munoz <bruno.munoz@orange.com>
2022-02-22 17:18:51 +0100

    add certs generation

---
+++ kind/kind-traefik-ingress/certs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# generate a host wildcard cert
+IP="192.168.56.101"
+mkcert "*.${IP}.nip.io" *.localhost localhost 127.0.0.1 ::1
+
+kubectl -n traefik create secret tls wildcard --cert=./_wildcard.${IP}.nip.io+4.pem --key=./_wildcard.${IP}.nip.io+4-key.pem
+
+kubectl -n default create secret tls keycloak-${IP}.nip.io-tls --cert=./_wildcard.${IP}.nip.io+4.pem --key=./_wildcard.${IP}.nip.io+4-key.pem
+
+#kubectl -n traefik apply -f default-cert.yaml
