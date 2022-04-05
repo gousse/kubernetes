7a2b546871da403f547117e645e99c46d0b7866b
Bruno Munoz <bruno.munoz@orange.com>
2022-04-05 12:01:22 +0200

    udpate timeout

--- kind/kind-traefik-ingress/1_deploy_traefik.sh
+++ kind/kind-traefik-ingress/1_deploy_traefik.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#TRAEFIK_HELM_VERSION="10.16.0"
+TRAEFIK_HELM_VERSION="10.19.0"
+
 # create namespace
 kubectl create namespace traefik --dry-run=client -o yaml | kubectl apply -f -
 
@@ -7,7 +10,13 @@ kubectl create namespace traefik --dry-run=client -o yaml | kubectl apply -f -
 bash ./certs.sh
 
 # helm traefik
-helm upgrade --install -n traefik traefik traefik/traefik -f traefik-values.yaml
+helm upgrade --install -n traefik traefik traefik/traefik -f traefik-values.yaml --version $TRAEFIK_HELM_VERSION
+
+sleep 5
+
+# wait for traefik CRD
+kubectl -n traefik wait --for condition=established --timeout=-1s crd tlsstores.traefik.containo.us
+
 # add a traefic monitoring service
 kubectl apply -n traefik -f monitoring-traefik.yaml
 
@@ -15,7 +24,7 @@ kubectl apply -n traefik -f monitoring-traefik.yaml
 kubectl apply -n traefik -f default-cert.yaml
 
 echo "Waiting for pods to be ready"
-kubectl wait --namespace traefik deploy/traefik --for condition=available
+kubectl wait --namespace traefik deploy/traefik --for condition=available --timeout=-1s
 
 pod=$(kubectl get pods -n traefik -l app.kubernetes.io/instance=traefik -o jsonpath='{.items[0].metadata.name}')
 kubectl -n traefik wait pod/${pod} --for condition=ready --timeout 120s
