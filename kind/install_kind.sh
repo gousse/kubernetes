70a99d6ed1785c07fe687d5523ff48a4b3f49239
Bruno Munoz <bruno.munoz@orange.com>
2022-09-07 16:26:11 +0200

    use new go install syntax

--- kind/install_kind.sh
+++ kind/install_kind.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-version=v0.11.1
-
-GO111MODULE="on" go get sigs.k8s.io/kind@${version}
-
-#warning no version is latest alpha 
+#version=v0.15.0
+#
+#GO111MODULE="on" go get sigs.k8s.io/kind@${version}
+#
+##warning no version is latest alpha 
 #$ go get sigs.k8s.io/kind
+
+
+
+go install sigs.k8s.io/kind@latest
