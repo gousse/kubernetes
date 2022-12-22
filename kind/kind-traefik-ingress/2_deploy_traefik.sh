f3ba11194f28b2f64790aa2f2881f16158eecd91
Bruno Munoz <bruno.munoz@orange.com>
2022-12-22 16:23:27 +0100

    fix name

--- kind/kind-traefik-ingress/2_deploy_traefik.sh
+++ kind/kind-traefik-ingress/2_deploy_traefik.sh
@@ -26,5 +26,5 @@ kubectl apply -n traefik -f default-cert.yaml
 echo "Waiting for pods to be ready"
 kubectl wait --namespace traefik deploy/traefik --for condition=available --timeout=-1s
 
-pod=$(kubectl get pods -n traefik -l app.kubernetes.io/instance=traefik -o jsonpath='{.items[0].metadata.name}')
+pod=$(kubectl get pods -n traefik -l app.kubernetes.io/name=traefik -o jsonpath='{.items[0].metadata.name}')
 kubectl -n traefik wait pod/${pod} --for condition=ready --timeout 120s
