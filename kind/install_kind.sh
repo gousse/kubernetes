b3d50914dd7c3c56727311eed807a93dafe1745f
Bruno Munoz <bruno@bmunoz.net>
2023-11-23 16:35:44 +0100

    cleanup

--- kind/install_kind.sh
+++ kind/install_kind.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 version=v0.19.0
-#
-#GO111MODULE="on" go get sigs.k8s.io/kind@${version}
-#
-##warning no version is latest alpha 
-#$ go get sigs.k8s.io/kind
-
+#version=v0.20.0
 
 go install sigs.k8s.io/kind@$version
 #go install sigs.k8s.io/kind@latest
