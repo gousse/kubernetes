db1354e3431a0ccc43719864e0415019d730b3e3
Bruno Munoz <bruno@bmunoz.net>
2024-01-15 17:23:40 +0100

    migrate traefik v3

--- kind/kind-traefik-ingress/2_deploy_traefik.sh
+++ kind/kind-traefik-ingress/2_deploy_traefik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #helm search repo traefik
-#TRAEFIK_HELM_VERSION="23.0.1"
-TRAEFIK_HELM_VERSION="23.1.0"
+#TRAEFIK_HELM_VERSION="23.1.0"
+TRAEFIK_HELM_VERSION="26.0.0"
 
 # create namespace
 kubectl create namespace traefik --dry-run=client -o yaml | kubectl apply -f -
@@ -15,7 +15,8 @@ helm upgrade --install -n traefik traefik traefik/traefik -f traefik-values.yaml
 sleep 5
 
 # wait for traefik CRD
-kubectl -n traefik wait --for condition=established --timeout=-1s crd tlsstores.traefik.containo.us
+#kubectl -n traefik wait --for condition=established --timeout=-1s crd tlsstores.traefik.containo.us
+kubectl -n traefik wait --for condition=established --timeout=-1s crd tlsstores.traefik.io
 
 # add a traefic monitoring service
 kubectl apply -n traefik -f monitoring-traefik.yaml
--- kind/kind-traefik-ingress/91_test_ingress.sh
+++ kind/kind-traefik-ingress/91_test_ingress.sh
@@ -3,27 +3,43 @@ LBFQDN="localhost"
 
 kubectl wait deploy/whoami-deployment --for condition=available --timeout=-1s
 
+echo "**********************"
 echo ${LBFQDN}/foo
 curl -s --noproxy '*' ${LBFQDN}/foo
+echo "**********************"
 
 echo ${LBFQDN}/bar
 curl -s --noproxy '*' ${LBFQDN}/bar
+echo "**********************"
 
 echo ${LBFQDN}/whoami
 curl -s --noproxy '*' ${LBFQDN}/whoami
+echo "**********************"
 
 echo https://${LBFQDN}/whoami
 curl -s --noproxy '*' https://${LBFQDN}/whoami
+echo "**********************"
+
+echo https://${LBFQDN}/echo
+curl -s --noproxy '*' https://${LBFQDN}/echo
+echo "**********************"
+
+echo https://${LBFQDN}/echo2
+curl -s --noproxy '*' https://${LBFQDN}/echo2
+echo "**********************"
 
 exit
 
 #LBFQDN="demo.192.168.56.101.nip.io"
-LBFQDN="echo.192.168.56.101.nip.io"
+#LBFQDN="echo.192.168.56.101.nip.io"
 #LBFQDN="localhost"
-
+#Verbose call 
 curl -sv --noproxy '*' ${LBFQDN}/foo
 curl -sv --noproxy '*' ${LBFQDN}/bar
 curl -sv --noproxy '*' ${LBFQDN}/whoami
 curl -sv --noproxy '*' ${LBFQDN}/echo
 curl -sv --noproxy '*' -k https://${LBFQDN}/echo
 curl -sv --noproxy '*' https://${LBFQDN}/echo
+curl -sv --noproxy '*' ${LBFQDN}/echo2
+curl -sv --noproxy '*' -k https://${LBFQDN}/echo2
+curl -sv --noproxy '*' https://${LBFQDN}/echo2
