4159fa4efcb069252ee384d745acb13bef967b42
Bruno Munoz <bruno@bmunoz.net>
2024-01-15 17:28:49 +0100

    migration v3, fix name

--- kind/kind-traefik-ingress/90_deploy_test.sh
+++ kind/kind-traefik-ingress/90_deploy_test.sh
@@ -2,4 +2,4 @@
 kubectl apply -f example1-foo-bar.yaml
 kubectl apply -f example2-whoami.yaml
 kubectl apply -f example3-http-https-echo-traefikSSLTerminaison.yaml
-kubectl apply -f example4-http-https-echo-sslpassthrough.yaml
+kubectl apply -f example4-http-https-passthrough.yaml
