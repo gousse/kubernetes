f332fec5a67446d4c6a1e0525919747da92f2cf1
Bruno Munoz <bruno@bmunoz.net>
2024-01-15 17:29:55 +0100

    cleanup

--- kind/kind-traefik-ingress/99_delete_test.sh
+++ kind/kind-traefik-ingress/99_delete_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 kubectl delete -f example1-foo-bar.yaml
 kubectl delete -f example2-whoami.yaml
-kubectl delete -f example3-http-https-echo-sslpassthrough.yaml
 kubectl delete -f example3-http-https-echo-traefikSSLTerminaison.yaml
-
+kubectl delete -f example4-http-https-passthrough.yaml
