5e1db977e23d8de98bcc34b839f8d34c79d078fd
Bruno Munoz <bruno@bmunoz.net>
2024-07-26 15:13:50 +0200

    update http-echo version

--- kind/kind-traefik-ingress/99_delete_test.sh
+++ kind/kind-traefik-ingress/99_delete_test.sh
@@ -3,3 +3,4 @@ kubectl delete -f example1-foo-bar.yaml
 kubectl delete -f example2-whoami.yaml
 kubectl delete -f example3-http-https-echo-traefikSSLTerminaison.yaml
 kubectl delete -f example4-http-https-passthrough.yaml
+kubectl delete -f example5_https2https_traefikfwd.sh
